5b77395e4fa30b58895a699c3ca752189f726fb2
peggylicious <p.asq@live.com>
2022-02-21 01:55:44 +0100

    Create new angular and add patient module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'patient', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
