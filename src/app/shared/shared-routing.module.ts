c1f1bccd023fe17337dfc13d24df6a3524da7549
peggylicious <p.asq@live.com>
2022-02-22 15:10:46 +0100

    Add login route for shared module

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { SharedComponent } from './shared.component';
 
-const routes: Routes = [{ path: '', component: SharedComponent }];
+const routes: Routes = [{ path: '', component: SharedComponent }, {path: 'login', component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
