12f2e4db5763554543e316f1204a8faf18221bec
peggylicious <p.asq@live.com>
2022-02-22 11:36:14 +0100

    Add shared module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'patient', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) }];
+const routes: Routes = [{ path: 'patient', loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) }, { path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
