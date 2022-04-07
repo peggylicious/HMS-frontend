be78e40b58742b0530cf90eae7b9b1f2d8f8f4c7
peggylicious <p.asq@live.com>
2022-04-07 16:49:24 +0100

    Add another server.js solution

--- server.js
+++ server.js
@@ -8,31 +8,48 @@
 // app.listen(process.env.PORT || 8080);
 
 // Redirect all traffic from http to https.
-const express = require('express');
-const app = express();
+// const express = require('express');
 // const app = express();
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-app.use(requireHTTPS);
+// // const app = express();
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+// app.use(requireHTTPS);
 
 
-// 2. Serve our static files.
-app.use(express.static('./dist/hms-frontend'));
+// // 2. Serve our static files.
+// app.use(express.static('./dist/hms-frontend'));
 
-// // 3. Wait for a request to any path and redirect all of the requests to index.html.
-// app.get(’/*’, function(req, res) {
-//   res.sendFile(’index.html’, {root: 'dist/<name-on-package.json>/’}
+// // // 3. Wait for a request to any path and redirect all of the requests to index.html.
+// // app.get(’/*’, function(req, res) {
+// //   res.sendFile(’index.html’, {root: 'dist/<name-on-package.json>/’}
+// // );
+// // });
+// app.get('/*', function(req, res) {
+//   res.sendFile('index.html', {root: 'dist/hms-frontend/'}
 // );
 // });
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/hms-frontend/'}
-);
+
+// // 4. Listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080.
+// app.listen(process.env.PORT || 8080);
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/hms-frontend'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/hms-frontend/index.html'));
 });
 
-// 4. Listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080.
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
