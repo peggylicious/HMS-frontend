ce40b2defa4a1f2eacedfc2db3f24f4cbee26c82
peggylicious <p.asq@live.com>
2022-04-07 14:01:04 +0100

    Add route: create-appointment

--- src/app/modules/patient-doctor/patient-doctor-routing.module.ts
+++ src/app/modules/patient-doctor/patient-doctor-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
 import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
 import { TimeSlotsComponent } from './pages/time-slots/time-slots.component';
 import { ViewDoctorDetailsComponent } from './pages/view-doctor-details/view-doctor-details.component';
@@ -8,7 +9,8 @@ import { PatientDoctorComponent } from './patient-doctor.component';
 const routes: Routes = [{ path: '', component: PatientDoctorComponent, children: [
   { path: 'view-doctor-details', component: ViewDoctorDetailsComponent },
   {path: 'set-appointment', component: TimeSlotsComponent}, 
-  {path: 'book-appointment', component: PatientDetailsComponent}
+  {path: 'book-appointment', component: PatientDetailsComponent},
+  {path: 'create-appointment', component: AppointmentsComponent}
 
 ] }];
 
