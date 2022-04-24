27da803bc48dd916462951802727ed21fe49d3a2
peggylicious <p.asq@live.com>
2022-04-24 23:29:58 +0100

    Add angular-jwt to auth service

--- src/app/core/authentication/auth.service.ts
+++ src/app/core/authentication/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,15 @@ export class AuthService {
 
   signup(data: any){
     console.log(data)
-    return this.http.post('https://peggy-hms-api.herokuapp.com/user/patient/signup', data)
+    return this.http.post('http://localhost:3000/user/patient/signup', data)
+    // return this.http.post('https://peggy-hms-api.herokuapp.com/user/patient/signup', data)
   }
 
   login(data: any){
+    let token = localStorage.getItem('token')
+    const headers = new HttpHeaders({ "Authorization": "Bearer "+token});
     console.log(data)
-    return this.http.post('https://peggy-hms-api.herokuapp.com/user/patient/login', data)
+    return this.http.post('http://localhost:3000/user/patient/login', data, {headers: {"Authorization": "Bearer "+token}})
+    // return this.http.post('https://peggy-hms-api.herokuapp.com/user/patient/login', data)
   }
 }
