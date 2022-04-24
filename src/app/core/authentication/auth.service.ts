af32ceba1b88abccab124d6192aaa0cecaadf822
peggylicious <p.asq@live.com>
2022-04-24 23:33:37 +0100

    Check if tocken is expired

--- src/app/core/authentication/auth.service.ts
+++ src/app/core/authentication/auth.service.ts
@@ -3,23 +3,29 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
-
-  signup(data: any){
-    console.log(data)
-    return this.http.post('http://localhost:3000/user/patient/signup', data)
+  constructor(private http: HttpClient, public jwtHelper: JwtHelperService) {}
+  public isAuthenticated(): boolean {
+    const token:any = localStorage.getItem('token');
+    // Check if token is expired and return
+    // true or false
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+  signup(data: any) {
+    console.log(data);
+    return this.http.post('http://localhost:3000/user/patient/signup', data);
     // return this.http.post('https://peggy-hms-api.herokuapp.com/user/patient/signup', data)
   }
 
-  login(data: any){
-    let token = localStorage.getItem('token')
-    const headers = new HttpHeaders({ "Authorization": "Bearer "+token});
-    console.log(data)
-    return this.http.post('http://localhost:3000/user/patient/login', data, {headers: {"Authorization": "Bearer "+token}})
+  login(data: any) {
+    let token = localStorage.getItem('token');
+    const headers = new HttpHeaders({ Authorization: 'Bearer ' + token });
+    console.log(data);
+    return this.http.post('http://localhost:3000/user/patient/login', data, {
+      headers: { Authorization: 'Bearer ' + token },
+    });
     // return this.http.post('https://peggy-hms-api.herokuapp.com/user/patient/login', data)
   }
 }
